a7fc1784589403e9fbdd6690656d0f1753ba443b
aristidei <a.isingizwe@alustudent.com>
2023-12-25 19:29:40 +0200

    privacy page

--- src/pages/Privacy.jsx
+++ src/pages/Privacy.jsx
@@ -1,3 +1,163 @@
 export default function Privacy() {
-  return <h1>this is privacy policy page</h1>;
+  return (
+    <article className="px-20 md:px-10 sm:px-6 text-lg text-black/70 py-6">
+      <p>
+        This Privacy Policy governs the manner in which Toovii collects, uses,
+        maintains and discloses information collected from users (each, a
+        “User”) of the Toovii website (“Site”). This privacy policy applies to
+        the Site and all products and services offered by Toovii.
+      </p>
+      <p className="font-bold text-xl text-black my-4">
+        Personal identification information
+      </p>
+      <p>
+        We may collect personal identification information from Users in a
+        variety of ways, including, but not limited to, when Users visit our
+        site, register on the site, place an order, subscribe to the newsletter,
+        respond to a survey, fill out a form, and in connection with other
+        activities, services, features or resources we make available on our
+        Site. Users may be asked for, as appropriate, name, email address,
+        mailing address, phone number, credit card information, social security
+        number. Users may, however, visit our Site anonymously. We will collect
+        personal identification information from Users only if they voluntarily
+        submit such information to us. Users can always refuse to supply
+        personal identification information, except that it may prevent them
+        from engaging in certain Site related activities.
+      </p>
+      <p className="font-bold text-xl text-black my-4">
+        Non-personal identification information
+      </p>
+      <p>
+        We may collect non-personal identification information about Users
+        whenever they interact with our Site. Non-personal identification
+        information may include the browser name, the type of computer and
+        technical information about users’ means of connection to our Site, such
+        as the operating system and the Internet service providers utilized and
+        other similar information.
+      </p>
+      <p className="font-bold text-xl text-black my-4">
+        Non-personal identification information
+      </p>
+      <p>
+        Toovii may collect and use Users personal information for the following
+        purposes:
+      </p>
+      <ul className="px-8 flex flex-col gap-6">
+        <li>
+          <div className="flex items-center gap-4">
+            <img className="w-5 object-contain" src="/minus.png" alt="" />
+            <p className="font-bold text-black text-lg">
+              To personalize user experience:{" "}
+            </p>
+          </div>
+          <p>
+            We may use information in the aggregate to understand how our Users
+            as a group use the services and resources provided on our Site.
+          </p>
+        </li>
+        <li>
+          <div className="flex items-center gap-4">
+            <img className="w-5 object-contain" src="/minus.png" alt="" />
+            <p className="font-bold text-black text-lg">To improve our site:</p>
+          </div>
+          <p>
+            We may use feedback you provide to improve our products and
+            services.
+          </p>
+        </li>
+        <li>
+          <div className="flex items-center gap-4">
+            <img className="w-5 object-contain" src="/minus.png" alt="" />
+            <p className="font-bold text-black text-lg">To process payments:</p>
+          </div>
+          <p>
+            We may use the information Users provide about themselves when
+            placing an order only to provide service to that order. We do not
+            share this information with outside parties except to the extent
+            necessary to provide the service.
+          </p>
+        </li>
+        <li>
+          <div className="flex items-center gap-4">
+            <img className="w-5 object-contain" src="/minus.png" alt="" />
+            <p className="font-bold text-black text-lg">
+              To run a promotion, contest, survey or other Site feature:
+            </p>
+          </div>
+          <p>
+            To send Users information they agreed to receive about topics we
+            think will be of interest to them.
+          </p>
+        </li>
+        <li>
+          <div className="flex items-center gap-4">
+            <img className="w-5 object-contain" src="/minus.png" alt="" />
+            <p className="font-bold text-black text-lg">
+              To send periodic emails:
+            </p>
+          </div>
+          <p>
+            We may use the email address to send User information and updates
+            pertaining to their order. It may also be used to respond to their
+            inquiries, questions, and/or other requests. If User decides to
+            opt-in to our mailing list, they will receive emails that may
+            include company news, updates, related product or service
+            information, etc. If at any time the User would like to unsubscribe
+            from receiving future emails, we include detailed unsubscribe
+            instructions at the bottom of each email.
+          </p>
+        </li>
+      </ul>
+      <p className="font-bold text-xl text-black my-4">Web browser cookies</p>
+      <p>
+        Our Site may use “cookies” to enhance User experience. User’s web
+        browser places cookies on their hard drive for record-keeping purposes
+        and sometimes to track information about them. Users may choose to set
+        their web browser to refuse cookies, or to alert you when cookies are
+        being sent. If they do so, note that some parts of the Site may not
+        function properly.
+      </p>
+      <p className="font-bold text-xl text-black my-4">
+        How we protect your information{" "}
+      </p>
+      <p>
+        We adopt appropriate data collection, storage and processing practices
+        and security measures to protect against unauthorized access,
+        alteration, disclosure or destruction of your personal information,
+        username, password, transaction information and data stored on our Site.
+      </p>
+      <p className="font-bold text-xl text-black my-4">
+        Sharing your personal information
+      </p>
+      <p>
+        We do not sell, trade, or rent Users personal identification information
+        to others. We may share generic aggregated demographic information not
+        linked to any personal identification information regarding visitors and
+        users with our business partners, trusted affiliates and advertisers for
+        the purposes outlined above. We may use third party service providers to
+        help us operate our business and the Site or administer activities on
+        our behalf, such as sending out newsletters or surveys. We may share
+        your information with these third parties for those limited purposes
+        provided that you have given us your permission. We may also release
+        your information when we believe release is appropriate to comply with
+        the law, enforce our site policies, or protect ours or others rights,
+        property, or safety. Additionally, non-personally identifiable visitor
+        information may be provided to other parties for marketing, advertising,
+        or other uses. We may also share your information with government
+        entities as required by law. Users have the right to request access to,
+        rectification or erasure of their personal data, and the right to
+        restrict or object to our processing of their personal data. Users also
+        have the right to data portability. Users may exercise these rights by
+        contacting us through the contact information provided at the end of
+        this policy. You should be aware that when you voluntarily disclose
+        personal information in a public area, such as on a message board, that
+        information can be collected and used by others and may result in
+        unsolicited messages. We are not responsible for the use of any personal
+        information that you disclose in a public area. We reserve the right to
+        update this policy at any time, so please review it frequently. If we
+        make material changes to this policy, we will notify you here, by email,
+        or through notice on our homepage.
+      </p>
+    </article>
+  );
 }
