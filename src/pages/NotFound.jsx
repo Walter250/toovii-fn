03560c56389b05c92fa9c2633b724972b11464de
aristidei <a.isingizwe@alustudent.com>
2024-01-05 15:43:20 +0200

    not found page styles

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,26 +1,21 @@
 import { Link } from "react-router-dom";
-import "../assets/notFound.css";
 export default function NotFound() {
   return (
-    <div id="oopss">
-      <div id="error-text">
-        <img
-          src="https://cdn.rawgit.com/ahmedhosna95/upload/1731955f/sad404.svg"
-          alt="404"
-        />
-        <span>404 PAGE</span>
-        <p className="p-a">
-          . The page you were looking for could not be found
-        </p>
-        <div className="text-base">
-          <Link
-            className="font-bold bg-black text-white px-10 py-2 rounded-full"
-            to="/"
-          >
-            Go Back Home
-          </Link>
-        </div>
-      </div>
-    </div>
+    <article className="bg-mainYellow h-screen w-full flex flex-col items-center gap-8 text-center px-6">
+      <img
+        src="https://cdn.rawgit.com/ahmedhosna95/upload/1731955f/sad404.svg"
+        alt="404"
+      />
+      <span className="font-bold text-5xl md:text-4xl sm:text-3xl">
+        404 PAGE
+      </span>
+      <p className="">The page you were looking for could not be found</p>
+      <Link
+        className="font-bold bg-black text-white px-10 py-2 rounded-full"
+        to="/"
+      >
+        Go Back Home
+      </Link>
+    </article>
   );
 }
